7b675e474ab12509583fe599581a1935c9264b54
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-10-06 12:15:40 +0500

    complete

--- ex42/Program.cs
+++ ex42/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ex42
 {
@@ -8,10 +9,12 @@ namespace ex42
         static void Main(string[] args)
         {
             const string CommandTakeCard = "1";
-            const string CommandExit = "2";
+            const string CommandDropCards = "2";
+            const string CommandReCreateDeck = "3";
+            const string CommandExit = "4";
 
             Player player = new Player();
-            Deck deck = new Deck();
+            Deck deck = new Deck(player);
 
             bool isOpen = true;
 
@@ -19,14 +22,23 @@ namespace ex42
             {
                 Console.SetCursorPosition(0, 10);
                 Console.WriteLine("Карты в руке:");
-                player.ShowHand();
+                player.ShowCardsInHand();
                 Console.SetCursorPosition(0, 0);
-                Console.Write($"{CommandTakeCard} - взять карту\n{CommandExit} - выйти из программы\nВведите команду: ");
+                Console.Write($"{CommandTakeCard} - Взять карты\n{CommandDropCards} - Сбросить карты в отбой\n" +
+                    $"{CommandReCreateDeck} - Пересоздать колоду\n{CommandExit} - Выход\nВыберете операцию: ");
 
                 switch (Console.ReadLine())
                 {
                     case CommandTakeCard:
-                        player.ReceiveCard(deck);
+                        player.TakeCard(deck);
+                        break;
+
+                    case CommandDropCards:
+                        player.DropCards();
+                        break;
+
+                    case CommandReCreateDeck:
+                        deck.GiveDeck(player);
                         break;
 
                     case CommandExit:
@@ -42,25 +54,24 @@ namespace ex42
 
     class Deck
     {
-        private List<Card> _cards = new List<Card>();
+        private List<Card> _deck = new List<Card>();
 
-        public Deck()
+        public Deck(Player player)
         {
-            CreateDeck();
-            ShuffleDeck();
+            GiveDeck(player);
         }
 
         public void CreateDeck()
         {
-            List<string> cardValues = new List<string> { "T", "6", "7", "8", "9", "10", "J", "D", "K" };
-            List<string> cardSuits = new List<string> { "♣", "♠", "♥", "♦" };
+            List<string> cardSuits = new List<string> { "♠", "♥", "♣", "♦" };
+            List<string> cardValues = new List<string> { "6", "7", "8", "9", "10", "J", "Q", "K", "T" };
 
-            for (int i = 0; i < cardValues.Count; i++)
+            for (int i = 0; i < cardSuits.Count; i++)
             {
-                for (int j = 0; j < cardSuits.Count; j++)
+                for (int j = 0; j < cardValues.Count; j++)
                 {
-                    Card card = new Card(cardValues[i], cardSuits[j]);
-                    _cards.Add(card);
+                    Card card = new Card(cardSuits[i], cardValues[j]);
+                    _deck.Add(card);
                 }
             }
         }
@@ -69,61 +80,113 @@ namespace ex42
         {
             Random random = new Random();
 
-            for (int i = 0; i < _cards.Count; i++)
+            for (int i = 0; i < _deck.Count; i++)
             {
-                int randomIndex = random.Next(0, _cards.Count);
-                (_cards[i], _cards[randomIndex]) = (_cards[randomIndex], _cards[i]);
+                int randomIndex = random.Next(i, _deck.Count);
+                (_deck[i], _deck[randomIndex]) = (_deck[randomIndex], _deck[i]);
             }
         }
 
         public Card GiveCard()
         {
-            Card lastCard = _cards[_cards.Count - 1];
-            _cards.Remove(lastCard);
-            return lastCard;
-        }
-    }
-
-    class Card
-    {
-        private string _value;
-        private string _suit;
-
-        public Card(string value, string suit)
-        {
-            _value = value;
-            _suit = suit;
+            if (_deck.Count > 0)
+            {
+                Card lastCard = _deck[_deck.Count - 1];
+                _deck.Remove(lastCard);
+                return lastCard;
+            }
+            else
+            {
+                Console.WriteLine("Карты в колоде закончились...");
+                return null;
+            }
         }
 
-        public void ShowInfo()
+        public int GiveNeedCardCount()
         {
-            Console.Write($"|{_value} {_suit}|");
+            Console.Write("Введите сколько карт вы хотите взять? ");
+            string cardCount = Console.ReadLine();
+
+            if (int.TryParse(cardCount, out int count))
+            {
+                if (count <= _deck.Count)
+                {
+                    return count;
+                }
+                else
+                {
+                    Console.WriteLine("В колоде недостаточно карт...");
+                    return 0;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод...");
+                return 0;
+            }
+        }
+
+        public void GiveDeck(Player player)
+        {
+            _deck.Clear();
+            player.DropCards();
+            CreateDeck();
+            ShuffleDeck();
         }
     }
 
     class Player
     {
-        private List<Card> _playerHand = new List<Card>();
+        private List<Card> _cardsInHand = new List<Card>();
 
-        public void ShowHand()
+        public void ShowCardsInHand()
         {
-            foreach (Card card in _playerHand)
+            foreach (Card card in _cardsInHand)
             {
                 card.ShowInfo();
             }
         }
 
-        public void ReceiveCard(Deck deck)
+        public void TakeCard(Deck deck)
         {
-            TakeCard(deck.GiveCard());
+            int count = deck.GiveNeedCardCount();
+
+            for (int i = 0; i < count; i++)
+            {
+                PutCardInHand(deck.GiveCard());
+            }
         }
 
-        public void TakeCard(Card card)
+        private void PutCardInHand(Card card)
         {
             if (card != null)
             {
-                _playerHand.Add(card);
+                _cardsInHand.Add(card);
             }
+        }
+
+        public void DropCards()
+        {
+            _cardsInHand.Clear();
+        }
+
+
+    }
+
+    class Card
+    {
+        private string _cardSuit;
+        private string _cardValue;
+
+        public Card(string cardSuit, string cardValue)
+        {
+            _cardSuit = cardSuit;
+            _cardValue = cardValue;
+        }
+
+        public void ShowInfo()
+        {
+            Console.Write($"|{_cardValue}{_cardSuit}|");
         }
     }
 }
